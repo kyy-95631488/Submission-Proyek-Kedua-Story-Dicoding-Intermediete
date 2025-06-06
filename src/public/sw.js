1d0d5853f0bf67dbeb2e5e8bdf29f89f60d20856
kyy-95631488 <riskir5622@gmail.com>
2025-06-06 12:32:45 +0700

    >> >> >> >> Update Commit [ 06 - 06 - 2025 | 12:32 ]

--- src/public/sw.js
+++ src/public/sw.js
@@ -12,7 +12,7 @@ const APP_SHELL = [
   { url: '/assets/index-cccboano.js', revision: '1' },
   // { url: '/scripts/index.js', revision: '1' },
   { url: '/assets/index-yrpki-zw.css', revision: '1' },
-  { url: '/styles/styles.css', revision: '1' },
+  // { url: '/styles/styles.css', revision: '1' },
   { url: '/favicon-192.png', revision: '1' },
   { url: '/favicon-512.png', revision: '1' },
   { url: '/favicon-96.png', revision: '1' },
